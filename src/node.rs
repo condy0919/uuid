1a5007461b5bdac04856804c693db89ccb6ec3b4
condy <condy0919@gmail.com>
2019-02-02 21:35:33 +0800

    support uuid.v1.node_id

---
+++ src/node.rs
@@ -0,0 +1,47 @@
+use std::convert::From;
+use std::fs;
+use std::path::Path;
+
+use crate::util::xtob;
+
+pub struct Node([u8; 6]);
+
+impl From<[u8; 6]> for Node {
+    fn from(xs: [u8; 6]) -> Self {
+        Node(xs)
+    }
+}
+
+impl Node {
+    #[cfg(target_os = "linux")]
+    pub fn new(interface: &str) -> Self {
+        let dir = "/sys/class/net";
+        let mut node: [u8; 6] = Default::default();
+
+        // aa:bb:cc:dd:ee:ff
+        let content = fs::read_to_string(Path::new(dir).join(interface).join("address"))
+            .unwrap_or("00:00:00:00:00:00".to_owned());
+
+        let bs = content.as_bytes();
+        for (idx, &x) in [0, 3, 6, 9, 12, 15].iter().enumerate() {
+            node[idx] = xtob(bs[x], bs[x + 1]).unwrap_or_default();
+        }
+
+        Node(node)
+    }
+
+    pub fn id(&self) -> [u8; 6] {
+        self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[cfg(target_os = "linux")]
+    fn test_lo_nodeid() {
+        assert_eq!(Node::new("lo").id(), [0, 0, 0, 0, 0, 0]);
+    }
+}
---
+++ src/v1.rs
@@ -0,0 +1,8 @@
+use super::*;
+use node::Node;
+
+impl Uuid {
+    pub fn new_v1() -> Uuid {
+
+    }
+}
