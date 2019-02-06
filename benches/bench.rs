b7991b104781c6301863635c2615ee996347ec54
condy <condy0919@gmail.com>
2019-02-07 03:34:49 +0800

    Add new_v1 benchmark comparasion

--- benches/bench.rs
+++ benches/bench.rs
@@ -42,6 +42,33 @@ fn benchmark_uuid_to_string(c: &mut Criterion) {
     });
 }
 
+// new_v1 benchmark group
+fn benchmark_yauuid_new_v1(c: &mut Criterion) {
+    c.bench_function("yauuid: new_v1", |b| {
+        b.iter(|| {
+            let mut ctx = yauuid::Context::new();
+            let node = yauuid::Node::new("lo");
+
+            let u = Uuid::new_v1(&mut ctx, &node);
+            assert_eq!(u.version(), yauuid::Version(1));
+            assert_eq!(u.variant(), yauuid::Variant::RFC4122);
+        })
+    });
+}
+
+fn benchmark_uuid_new_v1(c: &mut Criterion) {
+    c.bench_function("uuid: new_v1", |b| {
+        b.iter(|| {
+            let ctx = uuid::v1::Context::new(42);
+            let node = yauuid::Node::new("lo");
+
+            let u = uuid::Uuid::new_v1(&ctx, 1497624119, 1234, &node.id()).unwrap();
+            assert_eq!(u.get_version_num(), 1);
+            assert_eq!(u.get_variant().unwrap(), uuid::Variant::RFC4122);
+        })
+    });
+}
+
 // new_v3 benchmark group
 fn benchmark_yauuid_new_v3(c: &mut Criterion) {
     c.bench_function("yauuid: new_v3", |b| {
@@ -107,7 +134,8 @@ fn benchmark_uuid_new_v5(c: &mut Criterion) {
 
 criterion_group!(uuid_from_str, benchmark_yauuid_from_str, benchmark_uuid_from_str);
 criterion_group!(uuid_to_string, benchmark_yauuid_to_string, benchmark_uuid_to_string);
+criterion_group!(uuid_new_v1, benchmark_yauuid_new_v1, benchmark_uuid_new_v1);
 criterion_group!(uuid_new_v3, benchmark_yauuid_new_v3, benchmark_uuid_new_v3);
 criterion_group!(uuid_new_v4, benchmark_yauuid_new_v4, benchmark_uuid_new_v4);
 criterion_group!(uuid_new_v5, benchmark_yauuid_new_v5, benchmark_uuid_new_v5);
-criterion_main!(uuid_from_str, uuid_to_string, uuid_new_v3, uuid_new_v4, uuid_new_v5);
+criterion_main!(uuid_from_str, uuid_to_string, uuid_new_v1, uuid_new_v3, uuid_new_v4, uuid_new_v5);
