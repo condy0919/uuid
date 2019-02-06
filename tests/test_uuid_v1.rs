de0d2f4463498b1c6a4c4ccc90c1ff8a46af4449
condy <condy0919@gmail.com>
2019-02-07 00:13:20 +0800

    Add integration tests

--- tests/test_uuid_v1.rs
+++ tests/test_uuid_v1.rs
@@ -1,14 +1,14 @@
-extern crate uuid;
+extern crate yauuid;
 
-use uuid::{Context, Node};
-use uuid::Uuid;
+use yauuid::{Context, Node};
+use yauuid::Uuid;
 
 #[test]
 fn test_uuid_v1() {
     let mut ctx = Context::new();
     let node = Node::new("lo");
-    let uuid_v1_0 = Uuid::new_v1(&mut ctx, node);
-    let uuid_v1_1 = Uuid::new_v1(&mut ctx, node);
+    let uuid_v1_0 = Uuid::new_v1(&mut ctx, &node);
+    let uuid_v1_1 = Uuid::new_v1(&mut ctx, &node);
     assert_ne!(uuid_v1_0, uuid_v1_1);
 }
 
