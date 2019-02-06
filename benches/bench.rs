9dab880a031f9d6bfe0d08a64283bf2f2b598be0
condy <condy0919@gmail.com>
2019-02-07 00:13:32 +0800

    Add benchmarks

---
+++ benches/bench.rs
@@ -0,0 +1,32 @@
+#[macro_use]
+extern crate criterion;
+extern crate yauuid;
+extern crate uuid;
+
+use criterion::Criterion;
+use yauuid::Uuid;
+use std::str::FromStr;
+
+
+const BENCHMARK_STRING: &str = "f47ac10b-58cc-0372-8567-0e02b2c3d479";
+
+fn benchmark_from_str(c: &mut Criterion) {
+    c.bench_function("from_str", |b| {
+        b.iter(|| assert!(Uuid::from_str(BENCHMARK_STRING).is_ok()))
+    });
+}
+
+fn benchmark_new_v4(c: &mut Criterion) {
+    c.bench_function("new_v4", |b| {
+        b.iter(|| Uuid::new_v4());
+    });
+}
+
+fn benchmark_uuidrs_from_str(c: &mut Criterion) {
+    c.bench_function("uuid-rs/uuid from_str", |b| {
+        b.iter(|| assert!(uuid::Uuid::parse_str(BENCHMARK_STRING).is_ok()))
+    });
+}
+
+criterion_group!(benches, benchmark_from_str, benchmark_uuidrs_from_str, benchmark_new_v4);
+criterion_main!(benches);
