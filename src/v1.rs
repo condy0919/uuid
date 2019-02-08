fdc728009c2e46447bf679131854a3a7382952a2
condy <condy0919@gmail.com>
2019-02-08 23:02:37 +0800

    Accept some suggestions of clippy

--- src/v1.rs
+++ src/v1.rs
@@ -16,6 +16,7 @@ const g1582: u64 = epoch * 86400;
 const g1582ns100: u64 = g1582 * 1000000;
 
 /// A stateful context for the v1 generator
+#[derive(Default)]
 pub struct Context {
     last_time: u64,
     clock_seq: u16,
@@ -32,10 +33,7 @@ pub trait TimeClockSequence {
 impl Context {
     /// Creates a default context to help ensure uniqueness
     pub fn new() -> Self {
-        Context {
-            last_time: 0,
-            clock_seq: 0,
-        }
+        Default::default()
     }
 }
 
@@ -51,7 +49,7 @@ impl TimeClockSequence for Context {
 
             rng.fill_bytes(&mut bs);
 
-            self.clock_seq = ((((bs[0] as u16) << 8) | bs[1] as u16) & 0x3fff) | 0x8000; // Variant::RFC4122
+            self.clock_seq = (((u16::from(bs[0]) << 8) | u16::from(bs[1])) & 0x3fff) | 0x8000; // Variant::RFC4122
         }
 
         let now = since_the_epoch.as_secs() * 10000000
